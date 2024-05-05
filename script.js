126d704138a7312d208f9623dfa8fc88ccc34a66
Kuldeep kumar <117909055+kd-234@users.noreply.github.com>
2024-05-05 14:02:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function createTable() {
-    //Write your code here
-  
+    var rn = prompt("Input number of rows");
+    var cn = prompt("Input number of columns");
+    
+    var table = document.getElementById("myTable");
+    table.innerHTML = ''; // Clear previous content if any
+    
+    for (var i = 0; i < rn; i++) {
+        var row = table.insertRow(i);
+        for (var j = 0; j < cn; j++) {
+            var cell = row.insertCell(j);
+            cell.innerHTML = "Row-" + i + " Column-" + j;
+        }
+    }
 }
